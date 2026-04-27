27465c3faedf314b42a023ce211e26be824cb360
Дмитро Миронов <hattabith@gmail.com>
2026-04-27 23:20:03 +0300

    Add DB and init settings

---
+++ Backend/SmartHome.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SmartHome.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        // Тут будуть ваші таблиці. Наприклад, таблиця пристроїв:
+        // public DbSet<Device> Devices { get; set; }
+        // public DbSet<SensorReading> SensorReadings { get; set; }
+    }
+}
--- Backend/SmartHome.Api/Program.cs
+++ Backend/SmartHome.Api/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SmartHome.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 // Add services to the container.
 
